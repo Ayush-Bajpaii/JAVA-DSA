a450a4375faaa6d612c7450caa117963370798ee
Ayush-Bajpaii <bajpaiayush12345@gmail.com>
2025-04-08 11:41:00 +0530

    Initial commit

---
+++ src/LINKEDLIST/basicsll.java
@@ -0,0 +1,14 @@
+package LINKEDLIST;
+
+public class basicsll {
+
+    public static class Node{
+        int data;
+        Node next;
+    }
+
+    public static void main(String[] args) {
+        Node x = new Node();
+        System.out.println(x.next);
+    }
+}
