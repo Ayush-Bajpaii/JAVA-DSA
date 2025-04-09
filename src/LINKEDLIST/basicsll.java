4ed718920bbbb92d5fdca4e0e0205c11871669a0
Ayush-Bajpaii <bajpaiayush12345@gmail.com>
2025-04-09 10:58:03 +0530

    basic linked list

--- src/LINKEDLIST/basicsll.java
+++ src/LINKEDLIST/basicsll.java
@@ -5,10 +5,23 @@ public class basicsll {
     public static class Node{
         int data;
         Node next;
+        Node(int data){
+            this.data = data;
+        }
     }
 
     public static void main(String[] args) {
-        Node x = new Node();
-        System.out.println(x.next);
+        Node x = new Node(5);
+        Node y = new Node(6);
+        Node z = new Node(7);
+        Node a = new Node(8);
+        x.next = y;
+        y.next = z;
+        z.next = a;
+        System.out.println(x.next.data);
+        System.out.println(y.next.data);
+        System.out.println(z.next.data);
+
+
     }
 }
