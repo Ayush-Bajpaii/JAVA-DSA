17d70383b1df034e254e06bc4af59794c22e8497
Ayush-Bajpaii <bajpaiayush12345@gmail.com>
2025-04-10 10:37:16 +0530

    using for and while loop

--- src/LINKEDLIST/basicsll.java
+++ src/LINKEDLIST/basicsll.java
@@ -2,6 +2,17 @@ package LINKEDLIST;
 
 public class basicsll {
 
+    public static void display (Node head){
+     Node temp = head;
+     while(head != null ){
+         System.out.print(head.data + "  ");
+         head = head.next;
+     }
+
+
+        }
+
+
     public static class Node{
         int data;
         Node next;
@@ -11,16 +22,27 @@ public class basicsll {
     }
 
     public static void main(String[] args) {
-        Node x = new Node(5);
+        Node a = new Node(5);
         Node y = new Node(6);
         Node z = new Node(7);
-        Node a = new Node(8);
-        x.next = y;
+        Node b = new Node(8);
+        a.next = y;
         y.next = z;
-        z.next = a;
-        System.out.println(x.next.data);
-        System.out.println(y.next.data);
-        System.out.println(z.next.data);
+        z.next = b;
+//        System.out.println(x.next.data);
+//        System.out.println(y.next.data);
+//        System.out.println(z.next.data);
+//        System.out.println(x.next.next.data);
+        // Node temp = a;
+//        for(int i = 1;i<=4;i++){
+//            System.out.println(temp.data + " ");
+//            temp = temp.next;
+//        }
+//        while(temp !=null){ //printing the list
+//            System.out.print(temp.data + " ");
+//            temp = temp.next;
+//        }
+        display(a);
 
 
     }
