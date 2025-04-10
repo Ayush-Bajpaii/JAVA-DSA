e3174450576eff8b8d2dea09b1fd3c21b664da8e
Ayush-Bajpaii <bajpaiayush12345@gmail.com>
2025-04-10 11:31:43 +0530

    length of ll

--- src/LINKEDLIST/recursivell.java
+++ src/LINKEDLIST/recursivell.java
@@ -13,6 +13,17 @@ public class recursivell {
         System.out.print(head.data + " ");
         displayrll(head.next);
     }
+
+    public static int length(Node head){
+    int count = 0;
+    while(head != null){
+        count++;
+        head = head.next;
+    }
+    return count;
+    }
+
+
     public static class Node{
         int data;
         Node next;
@@ -26,12 +37,16 @@ public class recursivell {
         Node c = new Node(3);
         Node d = new Node(4);
         Node e = new Node(5);
+        Node f = new Node(100);
         a.next = b;
         b.next = c;
         c.next = d;
         d.next = e;
+        e.next = f;
         displayrll(a);
         System.out.println();
         displayrllr(a);
+        System.out.println();
+        System.out.println( length(a));
     }
 }
