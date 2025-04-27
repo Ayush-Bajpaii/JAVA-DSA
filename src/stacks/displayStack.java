2220127f5e89b511c2f798b02e3b7edc1205d2a5
Ayush-Bajpaii <bajpaiayush12345@gmail.com>
2025-04-27 15:50:05 +0530

    display recursively

--- src/stacks/displayStack.java
+++ src/stacks/displayStack.java
@@ -10,16 +10,26 @@ public class displayStack {
             stack.push(3);
             stack.push(4);
             stack.push(5);
-            Stack<Integer> rt = new Stack<>();
-            while(stack.size()>0){
-                rt.push(stack.pop());
-            }
-            while(rt.size()>0){
-                int x = rt.pop();
-                System.out.print(x+" ");
-                stack.push(x);
-            }
-        System.out.println(stack);
+//            Stack<Integer> rt = new Stack<>();
+//            while(stack.size()>0){
+//                rt.push(stack.pop());
+//            }
+//            while(rt.size()>0){
+//                int x = rt.pop();
+//                System.out.print(x+" ");
+//                stack.push(x);
+//            }
+//        System.out.println(stack);
+        int n = stack.size();
+        int[] arr = new int[n];
+        for(int i = n-1;i >= 0;i--){
+            arr[i] = stack.pop();
+
+        }
+        for (int i = 0;i<n;i++){
+            System.out.print(arr[i]+ " ");
+            stack.push(arr[i]);
+        }
 
     }
 }
